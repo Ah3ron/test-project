781f10f760315b9c39c0bc0f6dcdab946f4e00fd
Ah3ron <ah3ron@protonmail.com>
2023-03-08 04:53:55 +0300

    add hamburger menu for mobile

---
+++ script/menu.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector(".menu__button");
+const menu = document.querySelector(".menu");
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("menu_visible");
+
+    menuButton.disabled = true;
+    setTimeout( () => {
+        menuButton.disabled = false;
+    }, 100);
+});
+
+document.addEventListener("click", (e) => {
+    if (!menu.contains(e.target) && !menuButton.contains(e.target)) {
+        menu.classList.remove("menu_visible");
+    }
+});
